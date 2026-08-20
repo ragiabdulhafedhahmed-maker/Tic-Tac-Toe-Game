aec6a25bad14ce736bfd507c735f29bad1c62000
ragiabdulhafedhahmed-maker <ragiabdulhafedhahmed@gmail.com>
2026-08-20 23:48:15 +0300

    Fix Winning logic

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -64,7 +64,7 @@
             this.button1.TabIndex = 0;
             this.button1.Tag = "?";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
@@ -80,7 +80,7 @@
             this.button2.TabIndex = 1;
             this.button2.Tag = "?";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -96,7 +96,7 @@
             this.button3.TabIndex = 2;
             this.button3.Tag = "?";
             this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -112,7 +112,7 @@
             this.button4.TabIndex = 3;
             this.button4.Tag = "?";
             this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -128,7 +128,7 @@
             this.button5.TabIndex = 4;
             this.button5.Tag = "?";
             this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
@@ -144,7 +144,7 @@
             this.button6.TabIndex = 5;
             this.button6.Tag = "?";
             this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
@@ -160,7 +160,7 @@
             this.button7.TabIndex = 6;
             this.button7.Tag = "?";
             this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -176,7 +176,7 @@
             this.button8.TabIndex = 7;
             this.button8.Tag = "?";
             this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -192,7 +192,7 @@
             this.button9.TabIndex = 8;
             this.button9.Tag = "?";
             this.button9.UseVisualStyleBackColor = false;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // label1
             // 
